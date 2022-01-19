1194f697f14703259baf259ebad4c936d18784ea
Updesh <updesh7457@gmail.com>
2022-01-19 23:10:23 +0530

    Update code

--- Day 3 (19 Jan)/Sudoku.java
+++ Day 3 (19 Jan)/Sudoku.java
@@ -6,80 +6,75 @@
 // 2. Each column must contain the digits 1-9 without repetition.
 // 3. Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
 import java.util.*;
+
 public class Sudoku {
 
-    static boolean boxTest(int[][] sudoku,int rowIndex,int clmIndex){
-      
- 
-    return 
-    
-    
-    // if (val1==val2||val1==val3) {
-    //     return false;
-    // }
-    // else if (val2==val3) {
-        
-    // }
+    static boolean boxTest(int[][] sudoku, int rowIndex, int clmIndex) {
+
+        return true;
+
+        // if (val1==val2||val1==val3) {
+        // return false;
+        // }
+        // else if (val2==val3) {
+
+        // }
 
     }
+   
     static boolean isValidSudoku(int[][] sudoku) {
         int rowDgt, clmDgt;
-        boolean flg=true;
-        ArrayList<Integer>forRow = new ArrayList<>();
-        ArrayList<Integer>forcol = new ArrayList<>();
+       
+        HashSet<Integer> forRow = new HashSet<>();
+        HashSet<Integer> forcol = new HashSet<>();
+      
         for (int i = 0; i < sudoku.length; i++) {
             rowDgt = -1;
             clmDgt = -1;
-            
+
             for (int j = 0; j < sudoku.length; j++) {
 
-                if (sudoku[i][j] > 0) {
-                    if (!forRow.contains(sudoku[i][j])) {
-                        forRow.add(sudoku[i][j]);
+                if (forRow.contains(sudoku[i][j]))
+                    return false;
+                if (sudoku[i][j] != 0)
+                    forRow.add(sudoku[i][j]);
+
+                if (flgrow != 0) {
+                    for (int k = 0; k < sudoku.length; k++) {
+
+                        if (forcol.contains(sudoku[k][j]))
+                            return false;
+
+                        if (sudoku[i][j] != 0)
+                            forcol.add(sudoku[k][j]);
+
                     }
 
-                    else return false;
-
-                } 
-               
-
-                for (int k = i; k < sudoku.length; k++) {
-
-                    
-
-                    if (sudoku[k][j] > 0) {
-                        if(!forcol.contains(sudoku[k][j]));
-                        forcol.add(sudoku[k][j]);
-                    }
                 }
 
-                
-
-                 
-                if ((i+1) % 3 == 0 && (j+1) % 3 == 0) { 
-                    //System.out.println(i+" - "+j);
-                   // flg = boxTest(sudoku,i, j);
-
+                if ((i + 1) % 3 == 0 && (j + 1) % 3 == 0) {
+                    // System.out.println(i+" - "+j);
+                    // flg = boxTest(sudoku,i, j);
 
                 }
             }
 
         }
 
-        return flg;
+        return true;
     }
 
     public static void main(String[] args) {
-        int sudoku[][] = new int[][] { 
-                { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
-                { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
-                { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
-                { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
-                { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
-                { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
-                { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
-                { 0, 5, 0, 4, 1, 9, 0, 0, 5 },
-                { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
+        int sudoku[][] = new int[][] {
+            { 5, 3, 0, 0, 7, 0, 0, 0, 0 }, 
+            { 6, 0, 0, 1, 9, 0, 0, 0, 0 }, 
+            { 0, 9, 8, 0, 0, 0, 0, 9, 0 },
+            { 8, 0, 0, 0, 6, 0, 0, 0, 3 }, 
+            { 4, 0, 0, 8, 0, 3, 0, 0, 1 }, 
+            { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+            { 0, 6, 0, 0, 0, 0, 2, 8, 0 }, 
+            { 0, 0, 0, 4, 1, 9, 0, 0, 5 }, 
+            { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
         };
 
         System.out.println(isValidSudoku(sudoku));
--- Day 3 (19 Jan)/test.java
+++ Day 3 (19 Jan)/test.java
@@ -66,7 +66,7 @@ class test {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
 
-                    return cheackSudoku(matrix, i, j);
+                    if(!cheackSudoku(matrix, i, j)) return false;
             }
         }
         return true;
@@ -76,7 +76,7 @@ class test {
         int[][] board = new int[][] {
                 { 5, 3, 0, 0, 7, 0, 0, 0, 0 }, 
                 { 6, 0, 0, 1, 9, 0, 0, 0, 0 }, 
-                { 0, 9, 5, 0, 0, 0, 0, 9, 0 },
+                { 0, 1, 8, 0, 0, 0, 0, 9, 0 },
                 { 8, 0, 0, 0, 6, 0, 0, 0, 3 }, 
                 { 4, 0, 0, 8, 0, 3, 0, 0, 1 }, 
                 { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
