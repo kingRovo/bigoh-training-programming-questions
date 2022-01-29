27cb9bc7984bf0cabbeded72d8864646db268d5b
Updesh <updesh7457@gmail.com>
2022-01-29 19:08:27 +0530

    Done with week 2 questions

--- Week-2 Exam/BoxAdmine.java
+++ Week-2 Exam/BoxAdmine.java
@@ -2,89 +2,159 @@ import java.util.HashMap;
 
 interface MainBox {
 
-    public double area();
+    public void area();
 
-    public double pasting();
+    public int TotalGSMCost();
 
-    public double TotalGSMCost();
+    public int boxCost();
+
+
+
+    
 }
 
 class Box implements MainBox {
 
-    double hight;
-    double length;
-    double width;
+    int hight;
+    int length;
+    int width;
+    int totalGSMCost = 0;
 
-    double totalGSMCost = 0.0;
+    int extraGSM = 0;
 
-    double extraGSM = 0.0;
+    char flute;
+    int area = 0;
 
-    double PastingCoust;
-   Box(){
+    int pastingCost = 0;
 
-   }
-    Box(double hight, double length, double width,double PastingCoust) {
+    Box() {
+
+    }
+
+    Box(int hight, int length, int width) {
 
         this.hight = hight;
         this.length = length;
         this.width = width;
-        this.PastingCoust = PastingCoust;
+
     }
 
     @Override
-    public double area() {
+    public void area() {
 
-        return (hight * length * width);
-    }
-
-    @Override
-    public double pasting() {  
-        return PastingCoust;
-
+        area =  (hight * length * width);
     }
 
    
-    public void setGSMCost(double gsmCost) {
-        
-        totalGSMCost =  gsmCost*100;
+
+    public void setFlute(char flute) {
+
+        this.flute = flute;
+    }
+
+    public void setGSMCost(int gsmCost) {
+
+        totalGSMCost = gsmCost * 100;
+
+    }
+
+    public void isNeedOfExtraGSM(boolean extraGSM, HashMap<Character, Integer> flutes) {
+
+        if (extraGSM) {
+            this.extraGSM = flutes.get(flute);
+        }
+
     }
 
     @Override
-    public double TotalGSMCost() {
-        // TODO Auto-generated method stub
-        return  totalGSMCost+extraGSM;
+    public int TotalGSMCost() {
+
+        return totalGSMCost + extraGSM;
+    }
+
+    public void setPastingCost(int pastingCost) {
+        this.pastingCost = pastingCost;
+    }
+
+    public void boxPart(String parttype){
+
+        if(parttype == "Double"){
+
+            pastingCost =  pastingCost*3;
+        }
+    }
+
+    @Override
+    public int boxCost() {
+
+        // System.out.println("area = "+area);
+        // System.out.println("totalGSMCost = "+totalGSMCost);
+        // System.out.println("extraGSM = "+extraGSM);
+        // System.out.println("pastingCost = "+pastingCost);
+
+        return (area * totalGSMCost * extraGSM * pastingCost);
+
+
     }
 
 }
-class Universal extends Box{
 
-    Universal(double hight, double length, double width,double PastingCoust){
-        super(hight,length,width,PastingCoust);
+class Universal extends Box {
+
+
+
+    
+
+    Universal(int hight, int length, int width) {
+        
+        
+        super(hight, length, width);
+        
     }
 
-
-
 }
 
-class AllFlapMeeting extends Box{
+class AllFlapMeeting extends Box {
 
-}
-class HoneyComb extends Box{
     
-}
-class ReverseTuckIn extends Box{
-    
-}
-class TopOpening_SnapLock extends Box{
-    
+
+    AllFlapMeeting(int hight, int length, int width) {
+        
+        super(hight, length, width);
+    }
 }
 
+class HoneyComb extends Box {
+
+    HoneyComb(int hight, int length, int width) {
+        
+        super(hight, length, width);
+    }
+}
+
+class ReverseTuckIn extends Box {
+
+    ReverseTuckIn(int hight, int length, int width) {
+        
+        super(hight, length, width);
+    }
+
+}
+
+class TopOpening_SnapLock extends Box {
+
+    TopOpening_SnapLock(int hight, int length, int width) {
+        
+        super(hight, length, width);
+    }
+
+}
 
 public class BoxAdmine {
 
     public static void main(String[] args) {
-        
-        HashMap<Character,Integer> flute = new HashMap<>();
+
+        HashMap<Character, Integer> flute = new HashMap<>();
 
         flute.put('A', 10);
         flute.put('B', 30);
@@ -93,6 +163,24 @@ public class BoxAdmine {
         flute.put('F', 50);
         flute.put('G', 20);
 
-        Box universal = new 
+      
+
+        Box universal = new Universal(6, 9, 6);
+
+        // universal.setFlute('A');
+        // universal.setPastingCost(10);
+        // universal.isNeedOfExtraGSM(true, flute);
+        // universal.area();
+        // universal.boxPart("Double");
+        // universal.setGSMCost(12);
+        // System.out.println(universal.boxCost());
+
+
+        Box allFlapMeeting = new AllFlapMeeting(3,4,5);
+        Box honeyComb  = new HoneyComb(4,5,4);
+        Box reverseTuckIn = new ReverseTuckIn(3,4,4);
+        Box TopOpening_SnapLock = new TopOpening_SnapLock(3,4,4);
+
+
     }
 }
